de365bd8443c2c7cded2cedaa0e3ec5c1c5c8289
张晓旸 <zhangxiaoyang@news.cn>
2015-04-15 10:55:08 +0800

    add jason utils method

--- src/main/java/com/eudemon/taurus/app/json/JasonUtils.java
+++ src/main/java/com/eudemon/taurus/app/json/JasonUtils.java
@@ -2,6 +2,7 @@ package com.eudemon.taurus.app.json;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSON;
@@ -19,6 +20,21 @@ public class JasonUtils {
 			response.getWriter().println(jp);
 		} catch (IOException e) {
 			Log.getErrorLogger().error("write jasonp exception, rs : " + rs, e);
+		}
+	}
+	
+	public static void writeJasonP(HttpServletRequest request, HttpServletResponse response, Object data){
+		try {
+			String callback = request.getParameter("callback");
+			String jsn = JSON.toJSONString(data);
+			String jp = jsn;
+			if (null != callback && !callback.equals("")) {
+				jp = callback + "(" + jsn + ")";
+			}
+			response.setCharacterEncoding("utf-8");
+			response.getWriter().println(jp);
+		} catch (IOException e) {
+			Log.getErrorLogger().error("write jasonp exception, data : " + data, e);
 		}
 	}
 }
